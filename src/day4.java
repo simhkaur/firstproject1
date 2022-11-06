4c4a541238f72c52380161bd20d84b105d518ff7
simhkaur <rsimran1995@gmail.com>
2022-11-06 17:42:46 -0500

    jump 10 times

---
+++ src/day4.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class day4 {
+    public static void main(String[] args)
+    {
+        System.out.println("hello");
+        jump5Times();
+    }
+    public static void jump5Times()
+    {
+        System.out.println("jump 5 times");
+        System.out.println("jump 10 times");
+    }
+}
